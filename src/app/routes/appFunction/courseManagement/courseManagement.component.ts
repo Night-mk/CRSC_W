1a10b3ea5402343000821a0c6675ba8bdb1190e9
ldw1994 <1050082852@qq.com>
2018-06-12 21:12:09 +0800

    将将学校学院专业改成选择框

--- src/app/routes/appFunction/courseManagement/courseManagement.component.ts
+++ src/app/routes/appFunction/courseManagement/courseManagement.component.ts
@@ -1,18 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd';
-import { NzNotificationService } from 'ng-zorro-antd';
+// import { NzNotificationService } from 'ng-zorro-antd';
+
+
+
 
 @Component({
     selector: 'app-course-management',
     templateUrl: './courseManagement.component.html',
     styleUrls: ['./courseManagement.component.less']
 })
-export class CourseManagementComponent {
+export class CourseManagementComponent implements OnInit {
     form: FormGroup;
     submitting=false;
-    constructor(private http: _HttpClient,private fb: FormBuilder, private msg: NzMessageService,private notification: NzNotificationService) {
+    // selectedProvince = 'Zhejiang';
+    // selectedCity = 'Hangzhou';
+    // provinceData = [ 'Zhejiang', 'Jiangsu' ];
+    // cityData = {
+    //     Zhejiang: [ 'Hangzhou', 'Ningbo', 'Wenzhou' ],
+    //     Jiangsu : [ 'Nanjing', 'Suzhou', 'Zhenjiang' ]
+    // };
+    //
+    // provinceChange(value: string): void {
+    //     this.selectedCity = this.cityData[ value ][ 0 ];
+    // }
+
+    school= [
+        { index: 0, text: '福州大学', value: false, type: 'default', checked: false },
+        { index: 1, text: '福建师范大学', value: false, type: 'processing', checked: false },
+        { index: 2, text: '福建农林大学', value: false, type: 'success', checked: false },
+        { index: 3, text: '福建工程学院', value: false, type: 'error', checked: false }
+    ];
+
+    college=[
+        { index:0, text:'数计学院', value:false,type:'default', checked:false},
+        { index: 1, text: '经管学院', value: false, type: 'processing', checked: false },
+        { index: 2, text: '物信学院', value: false, type: 'success', checked: false },
+        { index: 3, text: '电气学院', value: false, type: 'error', checked: false }
+    ];
+
+    profession=[
+        { index:0, text:'数计学院', value:false,type:'default', checked:false},
+        { index: 1, text: '经管学院', value: false, type: 'processing', checked: false },
+        { index: 2, text: '物信学院', value: false, type: 'success', checked: false },
+        { index: 3, text: '电气学院', value: false, type: 'error', checked: false }
+    ]
+
+
+    constructor(private http: _HttpClient,private fb: FormBuilder, private msg: NzMessageService) {
     }
 
     ngOnInit(): void {
@@ -20,9 +57,6 @@ export class CourseManagementComponent {
             courseName: [ null, [ Validators.required ] ],
             courseID: [ null, [ Validators.required ] ],
             courseTime: [ null, [ Validators.required ] ],
-            school: [ null, [ Validators.required ] ],
-            college: [ null, [ Validators.required ] ],
-            profession: [ null, [ Validators.required ] ],
 
         });
     }
@@ -32,8 +66,8 @@ export class CourseManagementComponent {
     // }
     submit() {
         for (const i in this.form.controls) {
-            this.form.controls[i].markAsDirty();
-            this.form.controls[i].updateValueAndValidity();
+            this.form.controls[ i ].markAsDirty();
+            this.form.controls[ i ].updateValueAndValidity();
         }
         if (this.form.invalid) return;
         this.submitting = true;
@@ -42,5 +76,6 @@ export class CourseManagementComponent {
             this.msg.success(`提交成功`);
         }, 1000);
     }
+
 
 }
