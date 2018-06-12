12f180c733b2f8f07c0de2d81cf665bffd5e6e29
ldw1994 <1050082852@qq.com>
2018-06-12 12:03:33 +0800

    增加课程管理功能

--- src/app/routes/appFunction/courseManagement/courseManagement.component.ts
+++ src/app/routes/appFunction/courseManagement/courseManagement.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd';
+import { NzNotificationService } from 'ng-zorro-antd';
 
 @Component({
     selector: 'app-course-management',
@@ -7,7 +10,37 @@ import { _HttpClient } from '@delon/theme';
     styleUrls: ['./courseManagement.component.less']
 })
 export class CourseManagementComponent {
-    constructor(private http: _HttpClient) {
-
+    form: FormGroup;
+    submitting=false;
+    constructor(private http: _HttpClient,private fb: FormBuilder, private msg: NzMessageService,private notification: NzNotificationService) {
     }
+
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            courseName: [ null, [ Validators.required ] ],
+            courseID: [ null, [ Validators.required ] ],
+            courseTime: [ null, [ Validators.required ] ],
+            school: [ null, [ Validators.required ] ],
+            college: [ null, [ Validators.required ] ],
+            profession: [ null, [ Validators.required ] ],
+
+        });
+    }
+    // createNotification(type: string): void {
+    //     this.notification.create(type, '添加成功',
+    //         '恭喜添加了新的课程');
+    // }
+    submit() {
+        for (const i in this.form.controls) {
+            this.form.controls[i].markAsDirty();
+            this.form.controls[i].updateValueAndValidity();
+        }
+        if (this.form.invalid) return;
+        this.submitting = true;
+        setTimeout(() => {
+            this.submitting = false;
+            this.msg.success(`提交成功`);
+        }, 1000);
+    }
+
 }
